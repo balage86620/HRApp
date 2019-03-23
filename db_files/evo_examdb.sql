59086714341499cd5f72f01c1cdfb5ab5b2f474a
nOTESZ5 <notesz@tuta.io>
2019-03-23 16:42:04 +0100

    db files updated v2

--- db_files/evo_examdb.sql
+++ db_files/evo_examdb.sql
@@ -6,19 +6,11 @@ USE `evo_examdb` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `evo_examdb`.`hiring` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `evo_examdb`.`status`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `evo_examdb`.`status` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `date` DATE NULL DEFAULT NULL,
-  `description` VARCHAR(200) NULL DEFAULT NULL,
+  `job_name` VARCHAR(45) NULL,
+  `job_description` VARCHAR(2000) NULL,
+  `job_location` VARCHAR(45) NULL,
+  `salary_category` VARCHAR(500) NULL,
+  `job_id` INT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 3
@@ -46,20 +38,16 @@ DEFAULT CHARACTER SET = utf8;
 CREATE TABLE IF NOT EXISTS `evo_examdb`.`application` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `hiring_id` INT(11) NOT NULL,
-  `status_id` INT(11) NOT NULL,
   `person_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `hiring_id`, `status_id`, `person_id`),
+  `date_of_application` DATE NULL,
+  `status` VARCHAR(45) NULL,
+  `modification_date` DATE NULL,
+  PRIMARY KEY (`id`),
   INDEX `fk_application_hiring1_idx` (`hiring_id` ASC),
-  INDEX `fk_application_status1_idx` (`status_id` ASC),
   INDEX `fk_application_person1_idx` (`person_id` ASC),
   CONSTRAINT `fk_application_hiring1`
     FOREIGN KEY (`hiring_id`)
     REFERENCES `evo_examdb`.`hiring` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_application_status1`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `evo_examdb`.`status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_application_person1`
@@ -77,9 +65,11 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `evo_examdb`.`cv` (
   `id` INT(11) NOT NULL,
-  `path_to_file` VARCHAR(45) NULL DEFAULT NULL,
+  `path_to_file` VARCHAR(45) NOT NULL,
   `person_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `person_id`),
+  `language` VARCHAR(2) NOT NULL,
+  `upload_date` DATE NOT NULL,
+  PRIMARY KEY (`id`),
   INDEX `fk_cv_person1_idx` (`person_id` ASC),
   CONSTRAINT `fk_cv_person1`
     FOREIGN KEY (`person_id`)
@@ -110,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `evo_examdb`.`degree2person` (
   `date_of_issue` DATE NULL DEFAULT NULL,
   `degree_id` INT(11) NOT NULL,
   `person_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `degree_id`, `person_id`),
+  PRIMARY KEY (`id`),
   INDEX `fk_degree2person_degree_idx` (`degree_id` ASC),
   INDEX `fk_degree2person_person1_idx` (`person_id` ASC),
   CONSTRAINT `fk_degree2person_degree`
@@ -134,6 +124,7 @@ DEFAULT CHARACTER SET = utf8;
 CREATE TABLE IF NOT EXISTS `evo_examdb`.`skill` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL DEFAULT NULL,
+  `type` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 5
@@ -148,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `evo_examdb`.`skill2person` (
   `skill_id` INT(11) NOT NULL,
   `person_id` INT(11) NOT NULL,
   `skill_level` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`, `skill_id`, `person_id`),
+  PRIMARY KEY (`id`),
   INDEX `fk_skill2person_skill1_idx` (`skill_id` ASC),
   INDEX `fk_skill2person_person1_idx` (`person_id` ASC),
   CONSTRAINT `fk_skill2person_person1`
@@ -163,4 +154,73 @@ CREATE TABLE IF NOT EXISTS `evo_examdb`.`skill2person` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 AUTO_INCREMENT = 9
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `evo_examdb`.`interview`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evo_examdb`.`interview` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `date` DATE NULL DEFAULT NULL,
+  `description` VARCHAR(4000) NULL DEFAULT NULL,
+  `application_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_interview_application1_idx` (`application_id` ASC),
+  CONSTRAINT `fk_interview_application1`
+    FOREIGN KEY (`application_id`)
+    REFERENCES `evo_examdb`.`application` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `evo_examdb`.`skill2hiring`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evo_examdb`.`skill2hiring` (
+  `hiring_id` INT(11) NOT NULL,
+  `skill_id` INT(11) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `skill_level` INT(11) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_hiring_has_skill_skill1_idx` (`skill_id` ASC),
+  INDEX `fk_hiring_has_skill_hiring1_idx` (`hiring_id` ASC),
+  CONSTRAINT `fk_hiring_has_skill_hiring1`
+    FOREIGN KEY (`hiring_id`)
+    REFERENCES `evo_examdb`.`hiring` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_hiring_has_skill_skill1`
+    FOREIGN KEY (`skill_id`)
+    REFERENCES `evo_examdb`.`skill` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `evo_examdb`.`degree2hiring`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `evo_examdb`.`degree2hiring` (
+  `hiring_id` INT(11) NOT NULL,
+  `degree_id` INT(11) NOT NULL,
+  `id` INT NOT NULL,
+  INDEX `fk_hiring_has_degree_degree1_idx` (`degree_id` ASC),
+  INDEX `fk_hiring_has_degree_hiring1_idx` (`hiring_id` ASC),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_hiring_has_degree_hiring1`
+    FOREIGN KEY (`hiring_id`)
+    REFERENCES `evo_examdb`.`hiring` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_hiring_has_degree_degree1`
+    FOREIGN KEY (`degree_id`)
+    REFERENCES `evo_examdb`.`degree` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
--- db_files/inserts.sql
+++ db_files/inserts.sql
@@ -1,22 +1,25 @@
 INSERT INTO `evo_examdb`.`person` (`id`, `name`,`user_name`, `password`, `email`) VALUES ('1', 'Gipsz Jakab', 'gipszjakab1', '1234', 'gipszjakab@jakab.hu');
 INSERT INTO `evo_examdb`.`person` (`id`, `name`, `user_name`, `password`, `email`) VALUES ('2', 'Minta Márton', 'mintamarton2', '4321', 'mintamarton@gmail.com');
 
-INSERT INTO `evo_examdb`.`cv` (`id`, `path_to_file`, `person_id`) VALUES ('1', 'path', '1');
-INSERT INTO `evo_examdb`.`cv` (`id`, `path_to_file`, `person_id`) VALUES ('2', 'path 2', '2');
+INSERT INTO `evo_examdb`.`cv` (`id`, `path_to_file`, `person_id`, `language`, `upload_date`) VALUES ('1', 'path', '1', 'HU', '2018.03.23');
+INSERT INTO `evo_examdb`.`cv` (`id`, `path_to_file`, `person_id`, `language`, `upload_date`) VALUES ('2', 'path', '2', 'EN', '2018.03.23');
 
-INSERT INTO `evo_examdb`.`skill` (`id`, `name`) VALUES ('1', 'JavaEE');
-INSERT INTO `evo_examdb`.`skill` (`id`, `name`) VALUES ('2', 'JPA');
-INSERT INTO `evo_examdb`.`skill` (`id`, `name`) VALUES ('3', 'MYSQL');
-INSERT INTO `evo_examdb`.`skill` (`id`, `name`) VALUES ('4', 'REST');
+INSERT INTO `evo_examdb`.`skill` (`id`, `name`, `type`) VALUES ('1', 'JavaEE', 'Programming');
+INSERT INTO `evo_examdb`.`skill` (`id`, `name`, `type` ) VALUES ('2', 'JPA', 'Programming');
+INSERT INTO `evo_examdb`.`skill` (`id`, `name`, `type`) VALUES ('3', 'MYSQL', 'Database');
+INSERT INTO `evo_examdb`.`skill` (`id`, `name`, `type`) VALUES ('4', 'REST', 'Programming');
+INSERT INTO `evo_examdb`.`skill` (`id`, `name`, `type`) VALUES ('5', 'JavaSE', 'Programming');
 
 INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('1', '1', '1', '3');
 INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('2', '2', '1', '2');
 INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('3', '3', '1', '3');
 INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('4', '4', '1', '1');
-INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('5', '1', '2', '3');
-INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('6', '2', '2', '3');
-INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('7', '3', '2', '3');
-INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('8', '4', '2', '3');
+INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('5', '5', '1', '3');
+INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('6', '1', '2', '3');
+INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('7', '2', '2', '3');
+INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('8', '3', '2', '3');
+INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('9', '4', '2', '3');
+INSERT INTO `evo_examdb`.`skill2person` (`id`, `skill_id`, `person_id`, `skill_level`) VALUES ('10', '5', '2', '3');
 
 INSERT INTO `evo_examdb`.`degree` (`id`, `name`) VALUES ('1', 'BSC');
 INSERT INTO `evo_examdb`.`degree` (`id`, `name`) VALUES ('2', 'MSC');
@@ -24,11 +27,19 @@ INSERT INTO `evo_examdb`.`degree` (`id`, `name`) VALUES ('2', 'MSC');
 INSERT INTO `evo_examdb`.`degree2person` (`id`, `date_of_issue`, `degree_id`, `person_id`) VALUES ('1', '2015.06.05', '1', '1');
 INSERT INTO `evo_examdb`.`degree2person` (`id`, `date_of_issue`, `degree_id`, `person_id`) VALUES ('2', '2018.01.01', '2', '2');
 
-INSERT INTO `evo_examdb`.`status` (`id`, `date`, `description`) VALUES ('1', '2019.03.21', 'accepted');
-INSERT INTO `evo_examdb`.`status` (`id`, `date`, `description`) VALUES ('2', '2017.12.30', 'rejected');
+INSERT INTO `evo_examdb`.`hiring` (`id`, `job_name`, `job_description`, `job_location`, `salary_category`, `job_id`) VALUES ('1', 'Junior Java Fejlesztő', 'leírás', 'Budapest', '200000-400000 HUF', '1');
+INSERT INTO `evo_examdb`.`hiring` (`id`, `job_name`, `job_description`, `job_location`, `salary_category`, `job_id`) VALUES ('2', 'Senior JavaEE Developer', 'description', 'Berlin', '3000-6000 EUR', '2');
 
-INSERT INTO `evo_examdb`.`hiring` (`id`) VALUES ('1');
-INSERT INTO `evo_examdb`.`hiring` (`id`) VALUES ('2');
+INSERT INTO `evo_examdb`.`skill2hiring` (`id`, `hiring_id`, `skill_id`, `skill_level`) VALUES ('1', '1', '3', '2');
+INSERT INTO `evo_examdb`.`skill2hiring` (`id`, `hiring_id`, `skill_id`, `skill_level`) VALUES ('2', '1', '5', '3');
+INSERT INTO `evo_examdb`.`skill2hiring` (`id`, `hiring_id`, `skill_id`, `skill_level`) VALUES ('3', '2', '1', '3');
+INSERT INTO `evo_examdb`.`skill2hiring` (`id`, `hiring_id`, `skill_id`, `skill_level`) VALUES ('4', '2', '2', '3');
+INSERT INTO `evo_examdb`.`skill2hiring` (`id`, `hiring_id`, `skill_id`, `skill_level`) VALUES ('5', '2', '4', '3');
 
-INSERT INTO `evo_examdb`.`application` (`id`, `person_id`, `status_id`, `hiring_id`) VALUES ('1', '1', '2', '1');
-INSERT INTO `evo_examdb`.`application` (`id`, `person_id`, `status_id`, `hiring_id`) VALUES ('1', '2', '1', '2');
+INSERT INTO `evo_examdb`.`degree2hiring` (`id`, `hiring_id`, `degree_id`) VALUES ('1', '1', '1');
+INSERT INTO `evo_examdb`.`degree2hiring` (`id`, `hiring_id`, `degree_id`) VALUES ('2', '2', '2');
+
+INSERT INTO `evo_examdb`.`application` (`id`, `person_id`, `hiring_id`, `date_of_application`, `status`, `modification_date`) VALUES ('1', '1', '1', '2019.03.23', 'Open', '2019.03.23');
+INSERT INTO `evo_examdb`.`application` (`id`, `person_id`, `hiring_id`, `date_of_application`, `status`, `modification_date`) VALUES ('2', '2', '2', '2019.01.01', 'Closed', '2019.03.23');
+
+INSERT INTO `evo_examdb`.`interview` (`id`, `date`, `description`, `application_id`) VALUES ('1', '2019.03.25', 'Megfelelt!', '1');
